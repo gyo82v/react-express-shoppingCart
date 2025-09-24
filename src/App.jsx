79ac88af05c7727515f4498f84a87fd635dccfe8
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-09-24 17:07:30 +0200

    refactor: refactor tailwind in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,30 @@ import logo from "./images/turtle-bag.png"
 
 function App() {
   const [formdata, setFormdata] = useState({name : "", category : ""})
+  const [cart, setCart] = useState([])
 
   //tailwind
-  const container = `flex flex-col items-center bg-green-200 min-h-screen`
-  const main = `flex flex-col items-center mt-10 w-11/12`
+  const flex = `flex flex-col items-center`
+  const transition = `transition-transform transition-colors transition-shadow duration-300 ease-in-out
+                      shadow-lg shadow-green-700/30 w-full`
+  const container = `bg-green-200 min-h-screen`
+  const main = `mt-10 w-11/12`
   const logoImg = `w-28 h-28 rounded-lg mb-6`
-  const form = `flex flex-col items-center gap-4 p-4 w-full`
-  const input = `bg-green-100 p-2 rounded-lg text-green-700 font-semibold shadow-lg shadow-green-700/30 
-                 border-2 border-green-200 w-full
-                 transition-transform transition-colors transition-shadow duration-300 ease-in-out 
+  const form = `gap-4 p-4 w-full`
+  const input = `bg-green-100 p-2 rounded-lg text-green-700 font-semibold  
+                 border-2 border-green-200 
                  focus:outline-none focus:scale-105 focus:ring-2 focus:ring-lime-500 focus:shadow-xl
                  hover:border-lime-500 focus:bg-lime-100`
   const btn = `bg-gradient-to-br from-green-400 to-lime-200 text-green-600 font-bold 
-               rounded-lg shadow-lg px-4 py-2 w-full
-               transition-transform transition-colors transition-shadow duration-300 ease-in-out 
+               rounded-lg px-4 py-2 
                hover:scale-105 active:scale-95 hover:shadow-xl hover:from-lime-200 hover:to-green-400 
                hover:text-lime-700`
   const itemsSection = ``
   //
+
+  useEffect(() => {
+
+  }, [])
 
   const handleChange = e => {
     const {name, value} = e.target 
@@ -35,12 +41,12 @@ function App() {
 
 
   return (
-    <div className={container}>
-      <main className={main}>
+    <div className={`${flex} ${container}`}>
+      <main className={`${flex} ${main}`}>
         <img src={logo} alt="logo" className={logoImg} />
-        <form className={form} onSubmit={handleSubmit}>
+        <form className={`${flex} ${form}`} onSubmit={handleSubmit}>
           <input 
-            className={input} 
+            className={`${input} ${transition}`} 
             type="text"
             name="name"
             placeholder="Item name..."
@@ -50,7 +56,7 @@ function App() {
             required
           />
           <select 
-            className={`${input} bg-transparent appearance-none`}
+            className={`${input} ${transition} bg-transparent appearance-none`}
             name="category"
             value={formdata.category}
             onChange={handleChange}
@@ -65,7 +71,7 @@ function App() {
             <option value="electronic">Electronic</option>
             <option value="minsc">Minsc</option>
           </select>
-          <button className={btn} type="submit">Add to Cart</button>
+          <button className={`${transition} ${btn}`} type="submit">Add to Cart</button>
         </form>
         <section className={itemsSection}>
            <p>items array here</p>
